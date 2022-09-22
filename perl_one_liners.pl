dda019786ff28067b75c56db857689299715a01c
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2022-09-22 00:22:25 -0700

    Update perl_one_liners.pl

--- perl_one_liners.pl
+++ perl_one_liners.pl
@@ -2,3 +2,4 @@
 
 # Replace all instances of some word with another word in a given file
 perl -e 's/replacethis/withthis/g' myfile.txt
+# ^ Note: On Windows, replace single quotes with double quotes
