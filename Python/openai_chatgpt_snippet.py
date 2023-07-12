62a5810a884e895d06591fd03c8558beea08ad8e
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2023-07-11 23:29:59 -0700

    Create openai_chatgpt_snippet.py

---
+++ Python/openai_chatgpt_snippet.py
@@ -0,0 +1,17 @@
+import openai
+from dotenv import dotenv_values
+import json
+
+config = dotenv_values('.env');
+openai.api_key = config["OPENAI_API_KEY"]
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        { "role": "user", "content": "tell me a joke" }
+    ]
+)
+
+result = response.choices[0].message.content
+
+print(result)
