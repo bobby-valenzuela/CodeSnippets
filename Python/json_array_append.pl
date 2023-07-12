e3bb2d9d24725079cf8df88e4f288cc970b675d1
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2023-07-11 23:42:26 -0700

    Create json_array_append.pl

---
+++ Python/json_array_append.pl
@@ -0,0 +1,45 @@
+use JSON;
+use Data::Dumper;
+
+sub json_string_to_array
+{
+    my $json_string = $_[0];
+    my $json = JSON->new->allow_nonref;
+    my $json_hash_scalar = $json->decode( $json_string ); # # Same as calling decode_json($json_string)
+    my @json_hash = @$json_hash_scalar;
+    return @json_hash;
+}
+
+my $json_string = qq(
+	[
+		{
+			"name":"M.Scott",
+			"age": 0,
+			"hobbies": [
+				"hockey", "making threat level midnight","cheese puffs"
+			]	
+		},
+		{
+			"name":"M.Scott",
+			"age": 0,
+			"hobbies": [
+				"hockey", "making threat level midnight","cheese puffs"
+			]	
+		}
+	]
+);
+
+@json = json_string_to_array($json_string);
+
+$json[0]->{'Special Skills'} = "Sales";
+
+# Make JSON String
+$decoded = encode_json(\@json);
+
+print "JSON STRING => \t$decoded\n";
+
+# Make JSON Object
+$json_obj = to_json(\@json);
+
+print "JSON OBJECT => \t";
+print Dumper $json_obj;
